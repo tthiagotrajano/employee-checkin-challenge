b7d3d9d9918a3be99062d24804de0f941706f8f4
Thiago Trajano <72697364+tthiagotrajano@users.noreply.github.com>
2025-12-01 23:27:07 -0300

    Fix formatting of baseURL in api.js

--- employee-checkin-front/src/services/api.js
+++ employee-checkin-front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://employee-checkin-api.onrender.com"
+  baseURL: "https://employee-checkin-api.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
